d49e5966cd58451731c090262c5ce3fc6e6bb0a3
isabella <isabella@ISABELLA>
2026-08-11 14:04:04 -0600

    agregue estructura de menus de vuelos , reservas, reportes

--- src/Vista/frmMenu.java
+++ src/Vista/frmMenu.java
@@ -53,6 +53,7 @@ public class frmMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuItem1 = new javax.swing.JMenuItem();
         jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -64,8 +65,17 @@ public class frmMenu extends javax.swing.JFrame {
         menuItemUsuarios = new javax.swing.JMenuItem();
         menuItemClientes = new javax.swing.JMenuItem();
         menuVuelos = new javax.swing.JMenu();
+        menuItemAeropuertos = new javax.swing.JMenuItem();
+        menuItemRutas = new javax.swing.JMenuItem();
+        menuItemAviones = new javax.swing.JMenuItem();
+        menuItemTarifas = new javax.swing.JMenuItem();
         menuReservas = new javax.swing.JMenu();
+        menuItemAsientos = new javax.swing.JMenuItem();
+        menuItemReservas = new javax.swing.JMenuItem();
         menuReportes = new javax.swing.JMenu();
+        menuItemReportes = new javax.swing.JMenuItem();
+
+        jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Menu");
@@ -110,11 +120,11 @@ public class frmMenu extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(247, 247, 247)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 699, Short.MAX_VALUE)))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 258, Short.MAX_VALUE))
+                .addGap(287, 287, 287))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -122,9 +132,9 @@ public class frmMenu extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel3)
-                .addGap(37, 37, 37)
+                .addGap(35, 35, 35)
                 .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(66, Short.MAX_VALUE))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
@@ -173,16 +183,52 @@ public class frmMenu extends javax.swing.JFrame {
         menuVuelos.setForeground(new java.awt.Color(255, 255, 255));
         menuVuelos.setText("Vuelos");
         menuVuelos.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+
+        menuItemAeropuertos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemAeropuertos.setText("Aeropuertos");
+        menuVuelos.add(menuItemAeropuertos);
+
+        menuItemRutas.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemRutas.setText("Rutas");
+        menuVuelos.add(menuItemRutas);
+
+        menuItemAviones.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemAviones.setText("Aviones");
+        menuVuelos.add(menuItemAviones);
+
+        menuItemTarifas.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemTarifas.setText("Tarifas");
+        menuVuelos.add(menuItemTarifas);
+
         jMenuBar1.add(menuVuelos);
 
         menuReservas.setForeground(new java.awt.Color(255, 255, 255));
         menuReservas.setText("Reservas");
         menuReservas.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+
+        menuItemAsientos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemAsientos.setText("Asientos");
+        menuReservas.add(menuItemAsientos);
+
+        menuItemReservas.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemReservas.setText("Reservas");
+        menuReservas.add(menuItemReservas);
+
         jMenuBar1.add(menuReservas);
 
         menuReportes.setForeground(new java.awt.Color(255, 255, 255));
         menuReportes.setText("Reportes");
         menuReportes.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
+
+        menuItemReportes.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuItemReportes.setText("Reportes");
+        menuItemReportes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuItemReportesActionPerformed(evt);
+            }
+        });
+        menuReportes.add(menuItemReportes);
+
         jMenuBar1.add(menuReportes);
 
         setJMenuBar(jMenuBar1);
@@ -192,16 +238,12 @@ public class frmMenu extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
             .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 269, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -220,6 +262,10 @@ public class frmMenu extends javax.swing.JFrame {
     private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnSalirActionPerformed
+
+    private void menuItemReportesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuItemReportesActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menuItemReportesActionPerformed
 
     /**
      * @param args the command line arguments
@@ -262,9 +308,17 @@ public class frmMenu extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel jPanel1;
     public javax.swing.JMenu menuInicio;
+    public javax.swing.JMenuItem menuItemAeropuertos;
+    public javax.swing.JMenuItem menuItemAsientos;
+    public javax.swing.JMenuItem menuItemAviones;
     public javax.swing.JMenuItem menuItemClientes;
+    public javax.swing.JMenuItem menuItemReportes;
+    public javax.swing.JMenuItem menuItemReservas;
+    public javax.swing.JMenuItem menuItemRutas;
+    public javax.swing.JMenuItem menuItemTarifas;
     public javax.swing.JMenuItem menuItemUsuarios;
     public javax.swing.JMenu menuReportes;
     public javax.swing.JMenu menuReservas;
